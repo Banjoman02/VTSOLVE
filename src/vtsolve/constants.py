0fc2bdf5e9ebddb3f7bdf55315ebec9d5e7f51f5
Monty Campbell <mcampbell02@vt.edu>
2025-04-14 19:58:14 -0400

    Added more constants.

--- src/vtsolve/constants.py
+++ src/vtsolve/constants.py
@@ -4,8 +4,15 @@ Author: Monty Campbell
 Created: 2/3/2024
 Description: Contains a few important constants lol."""
 
+# Python Standard Imports
+from math import radians
+from datetime import datetime
+
 ECLIPTIC_INCLINATION: float = 0.4090926 # TODO: Cite this
 """``float``: Inclination of the ecliptic wrt the Earth's equator. Measured in radians."""
+
+J2000: datetime = datetime(2000, 1, 1, 0, 0, 0)
+"""``float``: Formal epoch definition of J2000."""
 
 G:float = 6.67430e-11 # TODO: Cite this
 """``float``: Gravity constant in N * m^2 / kg^2."""
@@ -21,3 +28,21 @@ MU_EARTH: float = G * M_EARTH
 
 MU_SUN: float = G * M_SUN
 """``float``: Gravity param for the sun."""
+
+EARTH_SMA: float = 149.598e6
+"""``float``: Earth orbital sma around sun."""
+
+EARTH_ECC:float = 0.01671022
+"""``float``: Earth's orbital eccentricity."""
+
+EARTH_INC: float = radians(0.00005)
+"""``float``: Earth inclination in radians."""
+
+EARTH_RAAN: float = radians(-11.26064)
+"""``float``: Earth right ascension of ascending node."""
+
+EARTH_ARGP: float = radians(102.94719)
+"""``float``: Earth argument of periapsis."""
+
+EARTH_MEAN_LONG: float = radians(100.46435)
+"""``float``: Earth's mean longitude."""
